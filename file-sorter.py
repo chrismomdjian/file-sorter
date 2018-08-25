beaade1d8fcfa7539b2e81917005b9b2cc159d22
chrismomdjian <chrismomdjian@gmail.com>
2018-08-24 22:16:23 -0700

    added condition to make sure not to move directories in current dir to destination folder

--- file-sorter.py
+++ file-sorter.py
@@ -4,9 +4,10 @@ import shutil
 destination = '/home/christian/Desktop/School/'
 number_of_transfers = 0
 
-# loop through all files in current dir and destination dir.
-files_in_current_dir = os.listdir( os.getcwd() )
+# get all files in current dir and destination dir.
+all_files_in_current_dir = os.listdir( os.getcwd() )
 files_in_destination = os.listdir( destination )
+files_in_current_dir = [ f for f in all_files_in_current_dir if os.path.isfile(f) ]
 
 # get first word of all filenames separated by underscore
 # and remove extensions
@@ -14,7 +15,7 @@ for f in files_in_current_dir:
     initial = f.split('_')
     no_ext = os.path.splitext( initial[0] )[0]
 
-    # if file matches a directory name, place the file in the directory.
+    # if beginning of filename matches beginning of a directory name, place that file in the directory.
     for fi in files_in_destination:
         if no_ext == fi.split('_')[0] and len(fi.split('.')) < 2:
             shutil.move(os.getcwd() + '/' + f, destination + fi)
